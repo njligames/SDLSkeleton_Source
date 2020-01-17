e251cc81e94dfcd73dcbc0ae4c890e223a941d93
James Folk <james_folk@discovery.com>
2020-01-17 15:47:06 -0500

    added test

---
+++ test/cpp/TestExample.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include "TestExample.h"
+
+#ifdef WRAP_EXAMPLES
+#include "allexamples/CurlExample.h"
+#endif
+
+//using namespace google_test_sample;
+using namespace std;
+
+TestExample::TestExample() {}
+
+TestExample::~TestExample() {};
+
+void TestExample::SetUp() {};
+
+void TestExample::TearDown() {};
+
