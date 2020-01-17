2a097f4af804030928d8652a94238a443594ea53
James Folk <james_folk@discovery.com>
2020-01-17 14:42:53 -0500

    first

---
+++ platform/android/com/example/streamingplayer/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.streamingplayer;
+
+import org.libsdl.app.SDLActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends SDLActivity
+{
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        System.out.println(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
