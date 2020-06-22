493275cdd93af84f0239fd8d440d778847e94614
James Folk <james@njligames.com>
2020-06-22 10:01:11 -0400

    updated profile

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -3,9 +3,12 @@
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
 
-mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+rm -Rf ~/Library/MobileDevice/Provisioning\ Profiles/*
+cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
 
-cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
+# mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+
+# cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
 #
 #
 # security create-keychain -p "" build.keychain
