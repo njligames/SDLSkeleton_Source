25b911c3b7d00ad50f8108dc812d44ee801d857c
James Folk <james@njligames.com>
2020-06-22 13:37:24 -0400

    sign

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -10,12 +10,12 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/25873690-a535-43a3-b6d8-88d9f3e28a87.mobileprovision
 
 
-security create-keychain -p "$IOS_PROFILE_KEY" build.keychain
-security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_PROFILE_KEY" -A
+security create-keychain -p "$CERTIFICATES_P12_PASSWORD" build.keychain
+security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$CERTIFICATES_P12_PASSWORD" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "$IOS_PROFILE_KEY" ~/Library/Keychains/build.keychain
+security unlock-keychain -p "$CERTIFICATES_P12_PASSWORD" ~/Library/Keychains/build.keychain
 
-security set-key-partition-list -S apple-tool:,apple: -s -k "$IOS_PROFILE_KEY" ~/Library/Keychains/build.keychain
+security set-key-partition-list -S apple-tool:,apple: -s -k "$CERTIFICATES_P12_PASSWORD" ~/Library/Keychains/build.keychain
 
