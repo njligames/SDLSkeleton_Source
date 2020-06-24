f3711c7d2171dddf9606f72742617ff21fc04b6e
James Folk <james@njligames.com>
2020-06-24 13:35:51 -0400

    updated the profiles

--- bin/ios.sh
+++ bin/ios.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 
-export XCODE_DEVELOPMENT_TEAM='SRBQ5SCF5X'
+export XCODE_DEVELOPMENT_TEAM="SRBQ5SCF5X"
+export XCODE_PRODUCT_BUNDLE_IDENTIFIER="com.njligames.sdlskeleton.ios.app"
+export CODE_SIGN_IDENTITY="Apple Development: James Folk (FWZPKCY3PH)"
+
 export EFFECTIVE_PLATFORM_NAME='-iphonesimulator'
 
 ./generate_project.sh ios
--- bin/test_macos.sh
+++ bin/test_macos.sh
@@ -2,36 +2,35 @@
 
 # /usr/bin/xcodebuild archive -project SDLSkeleton.xcodeproj -scheme SDLSkeleton -configuration "Release" -archivePath "./SDLSkeleton.xcarchive" DEVELOPMENT_TEAM="SRBQ5SCF5X" PROVISIONING_PROFILE_SPECIFIER="SDLSkeleton MacOS App Dev" CODE_SIGN_IDENTITY="Apple Development: James Folk (FWZPKCY3PH)"
 
-export XCODE_DEVELOPMENT_TEAM="SRBQ5SCF5X"
-export XCODE_PRODUCT_BUNDLE_IDENTIFIER="com.njligames.sdlskeleton.ios.app"
-
 set -x
 
-cd .build_ios
+cd .build_macos
 
-cmake --build .build_ios --config Release --target game-static
+# cmake --build . --config Release --target package
+cmake --build . --config Release --target game-static
 
 export XC_SCHEME='SDLSkeleton'
 export XC_CONFIGURATION='Release'
-export XC_WORKSPACE='SDLSkeleton.xcodeproj'
+export XC_PROJECT='SDLSkeleton.xcodeproj'
 export XC_ARCHIVE_PATH='./SDLSkeleton.xcarchive'
+export KEYCHAIN='login.keychain'
 
 /usr/bin/xcodebuild archive \
-    -project "$XC_WORKSPACE" \
+    -project "$XC_PROJECT" \
     -scheme "$XC_SCHEME" \
     -configuration "$XC_CONFIGURATION" \
     -archivePath "$XC_ARCHIVE_PATH" \
     DEVELOPMENT_TEAM="SRBQ5SCF5X" \
-    PROVISIONING_PROFILE_SPECIFIER="SDLSkeleton ios dev" \
+    PROVISIONING_PROFILE_SPECIFIER="SDLSkeleton macos dev" \
     CODE_SIGN_IDENTITY="Apple Development: James Folk (FWZPKCY3PH)"
 
 
 export XC_EXPORT_PATH='./artifacts/'
-export XC_EXPORT_OPTIONS_FILE_PATH='../../.github/data/ExportOptions.plist'
+export XC_EXPORT_OPTIONS_FILE_PATH='../ExportOptions.plist'
 
 /usr/bin/xcodebuild -exportArchive \
     -archivePath "$XC_ARCHIVE_PATH" \
     -exportOptionsPlist "$XC_EXPORT_OPTIONS_FILE_PATH" \
     -exportPath "$XC_EXPORT_PATH" \
     DEVELOPMENT_TEAM="SRBQ5SCF5X" \
-    PROVISIONING_PROFILE_SPECIFIER="SDLSkeleton ios dist adhoc"
+    PROVISIONING_PROFILE_SPECIFIER="SDLSkeleton macos dist"
