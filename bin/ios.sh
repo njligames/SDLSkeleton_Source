8390a5d0ed4a13f00cd432455be70588d672efb9
James Folk <james@njligames.com>
2020-07-01 17:35:29 -0400

    fixed the local build for ios

--- bin/ios.sh
+++ bin/ios.sh
@@ -4,6 +4,7 @@
 export XCODE_DEVELOPMENT_TEAM="SRBQ5SCF5X"
 export XCODE_PRODUCT_BUNDLE_IDENTIFIER="com.njligames.sdlskeleton.ios.app"
 export CODE_SIGN_IDENTITY="Apple Development: James Folk (FWZPKCY3PH)"
+export CODE_SIGN_STYLE="Automatic"
 
 export EFFECTIVE_PLATFORM_NAME='-iphonesimulator'
 
