a6d20ca337a7fec371ffcd8cc4c597093112a248
James Folk <james@njligames.com>
2020-07-02 12:24:54 -0400

    appletv now builds from comandline

--- bin/appletvos.sh
+++ bin/appletvos.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+export XCODE_DEVELOPMENT_TEAM="SRBQ5SCF5X"
+export XCODE_PRODUCT_BUNDLE_IDENTIFIER="com.njligames.sdlskeleton.appletvos.app"
+export CODE_SIGN_IDENTITY="Apple Development: James Folk (FWZPKCY3PH)"
+export CODE_SIGN_STYLE="Automatic"
+
+export EFFECTIVE_PLATFORM_NAME='-appletvsimulator'
+
 ./generate_project.sh appletv
 
--- bin/ios.sh
+++ bin/ios.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 export XCODE_DEVELOPMENT_TEAM="SRBQ5SCF5X"
 export XCODE_PRODUCT_BUNDLE_IDENTIFIER="com.njligames.sdlskeleton.ios.app"
 export CODE_SIGN_IDENTITY="Apple Development: James Folk (FWZPKCY3PH)"
